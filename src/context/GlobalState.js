8752beccfafe1c2bb6127e19bebf7444842fd27a
williampepple1 <36817380+williampepple1@users.noreply.github.com>
2021-01-17 02:56:50 +0100

    tracker

---
+++ src/context/GlobalState.js
@@ -0,0 +1,37 @@
+import React, { createContext, useReducer } from 'react';
+
+import AppReducer from './AppReducer';
+//initialState
+
+const initialState = {
+    transactions: []
+}
+
+//CreateContext
+export const GlobalContext = createContext(initialState);
+
+//Provider component
+export const GlobalProvider = ({ children }) => {
+ const [state, dispatch] = useReducer(AppReducer, initialState);
+
+function deleteTransaction(id) {
+    dispatch({
+        type: "DELETE_TRANSACTION",
+        payload: id
+    });
+}
+
+function addTransaction(transaction) {
+    dispatch({
+        type: "ADD_TRANSACTION",
+        payload: transaction
+    });
+}
+ return(<GlobalContext.Provider value={{
+     transactions: state.transactions,
+     deleteTransaction,
+     addTransaction
+ }}> 
+    {children}
+    </GlobalContext.Provider>)
+}
